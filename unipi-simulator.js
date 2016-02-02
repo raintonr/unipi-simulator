7a3c2ae80c65b83e4c911da4bc65afdb9612f00e
Robin Rainton <robin@rainton.com>
2016-02-02 17:05:34 +0100

    Start on frontend client

--- unipi-simulator.js
+++ unipi-simulator.js
@@ -1,13 +1,80 @@
-var WebSocketServer = require('websocket').server;
+var fs = require("fs");
 var http = require('http');
+var path = require("path");
+var WebSocketServer = require('websocket').server;
+
+/*
+ * We're going to create 2 web servers here:
+ * 1. For a user to connect to with a normal browser to trigger events
+ *    and see results.
+ * 2. For a control system to connect to. On this connection we
+ *    respond as if we are a UniPi Evok system.
+ * 
+ * Just for good measure we also handle STDIN keypresses, but they don't
+ * do much right now.
+ */
+
+/* Keep a list of all connects acting as users or Evok clients */
+ 
+var userClients = [];
+var evokClients = [];
+ 
+/* User Server */
+
+function userGet(req, res) {
+	var filePath = req.url == '/' ? 'frontend.html' : './' + req.url;
+	console.log("Serving up file: %s", filePath);
+
+	fs.exists(filePath, function(exists) {
+		if (!exists) {
+			res.statusCode = 404;
+			res.end("Ooops");
+		} else {
+			var stat = fs.statSync(filePath);
+			res.writeHead(200, {
+				"Content-Length" : stat.size
+			});
+
+			var readStream = fs.createReadStream(filePath);
+			readStream.pipe(res);
+		}
+	});
+}
+
+var userServer = http.createServer(function(req, res) {
+  userGet(req, res);
+}).listen(3031);
+
+new WebSocketServer({
+  httpServer: userServer
+}).on('request', function(request) {
+  console.log('New user connection from ', request.remoteAddress);
+  var connection = request.accept(null, request.origin);
+  userClients.push(connection);
+
+  connection.on('message', function(message) {
+    console.log('New user message: ', message);
+    /* Just send all WS messages to the Evok clients */
+    for (var lp = 0; lp < evokClients.length; lp++) {
+      evokClients[lp].sendUTF(message.utf8Data);
+    }
+  });
+
+  connection.on('close', function(connection) {
+    console.log('Closed connection');
+    /* TODO: remove the closed connection from our clients list */
+  });
+});
+
+
+console.log("Connect to web interface at http://localhost:3031/");
+
+/* Evok server */
 
 var server = http.createServer(function(request, response) {
   /* Don't do anything here, we're interested in WS only */
 });
 server.listen(3030, function() { });
-
-/* Let's keep a list of clients to send all inputs to */
-var clients = [];
 
 /* Store the value of 8 relays */
 var minRelay = 1;
@@ -27,15 +94,24 @@ function dumpRelays() {
 /*
  * WS server.
  */
+ 
 new WebSocketServer({
   httpServer: server
 }).on('request', function(request) {
   console.log('New connection from ', request.remoteAddress);
   var connection = request.accept(null, request.origin);
-  clients.push(connection);
+  evokClients.push(connection);
 
   connection.on('message', function(message) {
     console.log('New message: ', message);
+    
+    /* Send all WS messages to the User clients for display */
+    for (var lp = 0; lp < userClients.length; lp++) {
+      userClients[lp].sendUTF(message.utf8Data);
+    }
+    
+    /* Do our own primative display also */
+    
     data = JSON.parse(message.utf8Data);
     var dev = data.dev;
     var circuit = data.circuit;
@@ -69,8 +145,8 @@ function sendInput(circuit, value) {
   });
 
   console.log("Sending: ", json);
-  for (var lp = 0; lp < clients.length; lp++) {
-    clients[lp].sendUTF(json);
+  for (var lp = 0; lp < evokClients.length; lp++) {
+    evokClients[lp].sendUTF(json);
   }
 }
 
